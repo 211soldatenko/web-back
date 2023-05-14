a4085eaae17047a30ccc6350c370ff2b5e7244cc
211soldatenko <113393514+211soldatenko@users.noreply.github.com>
2023-05-14 17:38:08 +0300

    Add files via upload

--- web3/form.php
+++ web3/form.php
@@ -14,7 +14,7 @@
                 <form action="" method="POST">
                     <div class="form_block mod">
                         <h5>Ваше имя</h5>
-                        <input type="name" name="name" placeholder="Введите имя">
+                        <input type="name" name="name" placeholder="Введите имя" value="<?php print $values['name']; ?>">
                         <div class="error col-6">
                                 <?php
                                 if (!empty($messages['name']))
@@ -24,7 +24,7 @@
                     </div>
                     <div class="form_block mod">
                         <h5>Ваша почта</h5>
-                        <input type="email" name="email" placeholder="Введите эл. почту">
+                        <input type="email" name="email" placeholder="Введите эл. почту" value="<?php print $values['email']; ?>">
                         <div class="error col-6">
                                 <?php
                                 if (!empty($messages['email']))
@@ -34,7 +34,7 @@
                     </div>
                     <div class="form_block mod">
                         <h5>Дата рождения</h5>
-                        <input type="date" name="birthDate" value="1955-06-08">
+                        <input type="date" name="birthDate" value="1955-06-08" value="<?php print $values['birthDate']; ?>">
                         <div class="error col-6">
                                 <?php
                                 if (!empty($messages['birthDate']))
@@ -44,9 +44,13 @@
                     </div>
                     <div class="form_block ">
                         <h5>Пол</h5>
-                        <input type="radio" name="gender" value="Male">
+                        <input type="radio" name="gender" value="Male" <?php if ($values['gender'] == "Male") {
+                                    print 'checked';
+                                } ?>>
                         <span>Мужской  &nbsp&nbsp</span>
-                        <input type="radio" name="gender" value="Female">
+                        <input type="radio" name="gender" value="Female" <?php if ($values['gender'] == "Female") {
+                                    print 'checked';
+                                } ?>>
                         <span>Женский  &nbsp&nbsp</span>
                         <div class="form_error col-6">
                                 <?php
@@ -57,15 +61,25 @@
                     </div>
                     <div class="form_block">
                         <h5>Сколько конечностей</h5>
-                        <input type="radio" name="numOfLimbs" value="0">
+                        <input type="radio" name="numOfLimbs" value="0" <?php if ($values['numOfLimbs'] == "0") {
+                                    print 'checked';
+                                } ?>>
                         <span>0 &nbsp&nbsp</span>
-                        <input type="radio" name="numOfLimbs" value="1">
+                        <input type="radio" name="numOfLimbs" value="1" <?php if ($values['numOfLimbs'] == "1") {
+                                    print 'checked';
+                                } ?>>
                         <span>1 &nbsp&nbsp</span>
-                        <input type="radio" name="numOfLimbs" value="2">
+                        <input type="radio" name="numOfLimbs" value="2" <?php if ($values['numOfLimbs'] == "2") {
+                                    print 'checked';
+                                } ?>>
                         <span>2 &nbsp&nbsp</span>
-                        <input type="radio" name="numOfLimbs" value="3">
+                        <input type="radio" name="numOfLimbs" value="3" <?php if ($values['numOfLimbs'] == "3") {
+                                    print 'checked';
+                                } ?>>
                         <span>3 &nbsp&nbsp</span>
-                        <input type="radio" name="numOfLimbs" value="4">
+                        <input type="radio" name="numOfLimbs" value="4" <?php if ($values['numOfLimbs'] == "4") {
+                                    print 'checked';
+                                } ?>>
                         <span>4 &nbsp&nbsp</span>
                         <div class="error col-6">
                                 <?php
